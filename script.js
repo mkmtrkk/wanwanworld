3deac1a77b5c54d0945ab57c29f436e4376d6419
藤田 幸法 <straight.0429@gmail.com>
2022-06-17 17:29:22 +0900

    aboutにアニメーションを追加

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ $(function () {
             }
         });
     });
+
+    // 右と左からででくる
+    $(window).on('load scroll', function () {
+        $('.inview').each(function () {
+            //ターゲットの位置を取得
+            var target = $(this).offset().top;
+            //スクロール量を取得
+            var scroll = $(window).scrollTop();
+            //ウィンドウの高さを取得
+            var height = $(window).height();
+            //ターゲットまでスクロールするとフェードインする
+            if (scroll > target - height) {
+                //クラスを付与
+                $(this).addClass('is-show');
+            }
+        });
+    });
+
 });
+
